d567864f544effb2d0bca37d935b33b860e02f2d
Tope Taiwo <topzyray@yahoo.com>
2024-11-20 17:46:29 +0100

    Add state to sign up fields

--- frontend/lib/features/auth/view/pages/signup_page.dart
+++ frontend/lib/features/auth/view/pages/signup_page.dart
@@ -11,56 +11,77 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+    formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Sign Up",
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            CustomField(
-              hintText: "Name",
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            CustomField(
-              hintText: "Email",
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            CustomField(
-              hintText: "Password",
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            AuthGradientButton(),
-            const SizedBox(
-              height: 20,
-            ),
-            RichText(
-                text: TextSpan(
-                    text: "Already have an account? ",
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: [
-                  TextSpan(
-                      text: "Sign in",
-                      style: TextStyle(
-                          color: Pallete.gradient2,
-                          fontWeight: FontWeight.bold))
-                ]))
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomField(
+                hintText: "Name",
+                controller: nameController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomField(
+                hintText: "Password",
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              AuthGradientButton(),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "Already have an account? ",
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                    TextSpan(
+                        text: "Sign in",
+                        style: TextStyle(
+                            color: Pallete.gradient2,
+                            fontWeight: FontWeight.bold))
+                  ]))
+            ],
+          ),
         ),
       ),
     );
--- frontend/lib/features/auth/view/widgets/custom_field.dart
+++ frontend/lib/features/auth/view/widgets/custom_field.dart
@@ -2,12 +2,26 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String hintText;
-  const CustomField({super.key, required this.hintText});
+  final TextEditingController controller;
+  final bool isObscureText;
+  const CustomField(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      this.isObscureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(hintText: hintText),
+      validator: (val) {
+        if (val!.trim().isEmpty) {
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
     );
   }
 }
